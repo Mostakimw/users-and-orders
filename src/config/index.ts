3ee27c314c29d8742cc27fcb38dd17d38801a5f7
Mostakimw <scientist.expert@gmail.com>
2023-11-22 23:39:45 +0600

    eslint and prettier setup completed

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
-import dotenv from "dotenv";
-import path from "path";
+import dotenv from 'dotenv'
+import path from 'path'
 
 dotenv.config({
-  path: path.join(process.cwd(), ".env"),
-});
+  path: path.join(process.cwd(), '.env'),
+})
 
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
   database_url_local: process.env.DATABASE_URL_LOCAL,
-};
+}
--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,12 @@ async function server() {
       console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
+
+
+
+
+
+    
     console.log(error);
   }
 }
